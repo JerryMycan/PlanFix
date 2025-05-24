8affb8f15c690ea90d096e63518010d005779f1d
JarekMycan <geekom@home.com>
2025-05-24 15:33:21 +0200

    Nachtrag zu dynamischen Umgebung

--- pf-importlehrkraefte/lehrkraefte_edit.php
+++ pf-importlehrkraefte/lehrkraefte_edit.php
@@ -5,10 +5,10 @@
 
 // Dynamischer Pfad zur db_connection.php je nach Umgebung
 $basePath = (strpos(gethostname(), 'geekom') !== false)
-    ? '/var/www/html/help/' // lokale Umgebung zu Hause
+    ? '/var/www/html/planfix/' // lokale Umgebung zu Hause
     : '/var/www/planfix.kleist-schule.de/web/'; // Server
 
-require_once $basePath . 'includes/db_connection.php';
+require_once $basePath . 'pf-importlehrkraefte/includes/db_connection.php';
 
 // Lehrkräfte abrufen, alphabetisch nach Kürzel sortiert
 $sql = "SELECT * FROM lehrkraefte ORDER BY kuerzel ASC";
