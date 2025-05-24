e547abd140972208141334428e3ae935ed883bbe
JarekMycan <geekom@home.com>
2025-05-24 16:28:19 +0200

    db_config.php ins Repository !

---
+++ pf-importlehrkraefte/includes/db_config.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_LOCAL_HOST', '127.0.0.1');
+define('DB_LOCAL_USER', 'root');
+define('DB_LOCAL_PASSWORD', 'Kym7HEbeS6#');
+define('DB_LOCAL_NAME', 'c1_planfix');
+
+define('DB_SERVER_HOST', 'localhost');
+define('DB_SERVER_USER', 'c1sltadmin');
+define('DB_SERVER_PASSWORD', 'dnE3@JyA');
+define('DB_SERVER_NAME', 'c1_planfix');
+?>
